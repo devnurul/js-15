75f5422bc04266c661bc3db2ebb2cb4bf4808466
freelancernoor <info@devnurul.me>
2021-09-10 05:01:00 +0600

    array

--- arrAdd.js
+++ arrAdd.js
@@ -1,14 +1,38 @@
 
-function arrayjog (num){
-    var sum = 0;
+// function arrayjog (num){
+//     var sum = 0;
 
-    for( var i= 0; i <num.length; i++){
-    var element = num[i];
-        sum = sum + element;
+//     for( var i= 0; i <num.length; i++){
+//     var element = num[i];
+//         sum = sum + element;
+//     }
+//     return sum
+// }
+// var num = [45, 48, 42, 54, 62, 16];
+// var result = arrayjog(num)
+// console.log(result);
+
+// var numbers = [4, 8, 5, 6, 4, ];
+
+// var sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i] + sum;
+//     sum = element;
+    
+// }
+// console.log(sum);
+
+// functions
+
+function addArrayNumber(numbers){
+    var sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        sum += element;
     }
     return sum
 }
-var num = [45, 48, 42, 54, 62, 16];
-var result = arrayjog(num)
-console.log(result);
+ var Numbers = [1, 2, 3, 4 ,5]
 
+ var result = addArrayNumber(Numbers);
+ console.log(result);
--- prob_5.js
+++ prob_5.js
@@ -1,10 +1,21 @@
-var marks = [63, 45, 82, 14, 29, 38, 99 ,10]
+// var marks = [63, 45, 82, 14, 29, 38, 99 ,10]
 
-var max = marks[0];
-for(var i =0; i < marks.length; i++){
-    var element = marks[i];
-    if(element >max){
-        max= element;
+// var max = marks[0];
+// for(var i =0; i < marks.length; i++){
+//     var element = marks[i];
+//     if(element >max){
+//         max= element;
+//     }
+// }
+// console.log("This is height number= ", max );
+
+var mark = [63, 51, 25, 15, 14, 48, 58, 59, 54, 10];
+
+var max = mark [0];
+for (let i = 0; i <mark.length; i++) {
+    const element =mark[i]
+    if (element > max) {
+        max = element;     
     }
 }
-console.log("This is height number= ", max );
+console.log(max);  
